a53245ea6c08de11b7c7f9f2f7c19fbf8a7f1c70
Antonin Blanc <antonin.blanc@ipsosante.fr>
2016-02-19 17:26:38 +0100

    Bump 0.13.0-ipso.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from tastypie import __version__
 
 setup(
     name='django-tastypie',
-    version='0.13.0-ipso.1',
+    version='0.13.0-ipso.2',
     description='A flexible & capable API layer for Django.',
     author='Daniel Lindsley',
     author_email='daniel@toastdriven.com',
