a9668bd8f0c32a023339e7da781ebfef0119cc06
Antonin Blanc <antonin.blanc@ipsosante.fr>
2016-05-20 13:21:16 +0200

    Bump 0.14.0-ipso.1

--- tastypie/__init__.py
+++ tastypie/__init__.py
@@ -3,7 +3,8 @@ from __future__ import unicode_literals
 
 __author__ = 'Daniel Lindsley & the Tastypie core team'
 
-VERSION = (0, 14, 0)
 
-__short_version__ = '.'.join(map(str, VERSION[0:2]))
-__version__ = ''.join(['.'.join(map(str, VERSION[0:3])), ''.join(VERSION[3:])])
+VERSION = "0.14.0-ipso.1"
+
+__short_version__ = '.'.join(map(str, VERSION.split('.')[0:2]))
+__version__ = VERSION
